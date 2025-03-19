502b82a90eed807843fc4109de2970322dceadbc
rajeshn67 <rajeshnarwade67@gmail.com>
2025-03-19 21:29:23 +0530

    added

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectDB = async()=>
 {
      await mongoose.connect(
-       // "mongodb+srv://firstweb:GSQ9fjFvs6nsvKya@firstweb.9iplm.mongodb.net/FirstDatabase"
+        "mongodb+srv://firstweb:GSQ9fjFvs6nsvKya@firstweb.9iplm.mongodb.net/FirstDatabase"
      );
 };
  
