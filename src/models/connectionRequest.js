b7cd7bbfd37ccf8adbe8c0d6ff68893ede807fd7
rajeshn67 <rajeshnarwade67@gmail.com>
2025-03-20 16:19:02 +0530

    added

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -22,5 +22,13 @@ const connectionRequestSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+connectionRequestSchema.pre("save",function(next){
+   const connectionRequest=this;
+   //Check if the fromuserId is same as toUserId
+   if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+    throw new Error("Cannot send connection request to yourself");
+   }
+next();
+});
 const ConnectionRequestModel = mongoose.model( "ConnectionRequest", connectionRequestSchema);
 module.exports=ConnectionRequestModel;
--- src/routes/request.js
+++ src/routes/request.js
@@ -2,6 +2,8 @@ const express =require("express");
 const requestRouter=express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnnectionRequest=require("../models/connectionRequest");
+const Usermodel=require("../models/user");
+
 requestRouter.post("/request/send/:status/:toUserId",userAuth ,async(req,res)=>
     {
       try{
@@ -12,14 +14,39 @@ const allowedStatus=["ignored","interested"];
         if(!allowedStatus.includes(status)){
            return res.status(400).json({ message: "Invalid status type :"+status });
         }
+
+        const toUser = await Usermodel.findById(toUserId);
+        if(!toUser){
+          return res.status(404).json({ message: "user not found" });
+
+        }
+
+//if there is an existing connectionrequest
+const existingConnectionRequest = await ConnnectionRequest.findOne({
+   $or: [
+       {fromUserId,toUserId}, 
+       {fromUserId:toUserId, toUserId:fromUserId }
+      ],
+});
+
+ if(existingConnectionRequest){
+    return res
+      .status(400)
+      .json({
+        message: "connection request already exists",
+      });
+ }
         const ConnectionRequest= new ConnnectionRequest({
-           fromUserId,toUserId,status,
+           fromUserId,
+           toUserId,
+           status,
         });
 
         const data=await ConnectionRequest.save();
         res.json({
-           message:"Connection request sent Successfully",
-           data,
+          message:
+            req.user.firstname +" is " + status +" in " + toUser.firstname,
+          data,
         });
       }
        catch(err){
