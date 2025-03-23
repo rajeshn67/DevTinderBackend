700be74cfd61842962f7f3c47288d3040a98d5c0
rajeshn67 <rajeshnarwade67@gmail.com>
2025-03-23 12:23:49 +0530

    added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
-
+const cors=require("cors");
 const cookieParser = require("cookie-parser");
 
+app.use(
+  cors({
+    origin: "http://localhost:5173", // backend should know where the frontend is running
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -45,7 +45,7 @@ authRouter.post("/login", async (req, res) => {
       //add the token to cookie and send the response back to the user
 
       res.cookie("token", token, { expires: new Date(Date.now() + 1*3600000)});
-      res.send("Login successfully");
+      res.send(user);
     } else {
       throw new Error("Invalid credentials");
     }
