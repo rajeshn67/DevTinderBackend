66cbc05dd622eddaf10ad614996d708038f981a9
rajeshn67 <rajeshnarwade67@gmail.com>
2025-05-08 23:15:40 +0530

    Added

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,7 @@ const express =require("express");
 const authRouter=express.Router();
 const { validatesignUpData } = require("../utils/validation");
 const Usermodel = require("../models/user");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 authRouter.post("/signup", async (req, res) => {
   try {
